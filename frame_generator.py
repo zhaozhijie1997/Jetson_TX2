528535d6f556519f610c1fe7195ecbeca047b5da
Zhao Zhijie <52944822+zhaozhijie1997@users.noreply.github.com>
2019-10-09 12:09:53 +0800

    Add files via upload

--- frame_generator.py
+++ frame_generator.py
@@ -4,22 +4,33 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--video", type=str, help="Video file name here")
-args = parser.parse_args()
+parser.add_argument("--output", type=str, help="Jpg output name here")
+parser.add_argument("--frames", type=int, default=2, help="no of images per second ")
+args, unknown = parser.parse_known_args()
 
 
 
 
-cv2.VideoCapture(args.video)
+get = args.frames
+
+to_get = int(10/get)
+
+name = args.output
+cap = cv2.VideoCapture(args.video)
 count = 0
 frame_no = 0
-while(True):
+while(cap.isOpened()):
 	ret, frame = cap.read()
-	count = count+1
-	if count%5==0 :
-		frame_no +=
-		img = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-		cv2.imwrite('data/frame{}'.format(frame_no), img)
+	count = count + 1
+	if ret == True:
+		if count%to_get==0 :
+			frame_no = frame_no + 1
+			#img = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+			cv2.imwrite('../data/{}.jpg'.format(name+str(frame_no)), frame)
+			count=0
+	else:
+		break
 
-
+cap.release()
 cv2.destroyAllWindows()
 
